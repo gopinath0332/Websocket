3e2d10fd17cbb548f875eeeeb8ee3b99589bc037
Gopi <gopinath0332@outlook.com>
2016-03-21 23:53:44 +0530

    Changing server to broadcast message to all the connected users.

--- src/main/java/com/sample/websocket/service/WebsocketServer.java
+++ src/main/java/com/sample/websocket/service/WebsocketServer.java
@@ -1,4 +1,12 @@
+/**
+ * Websocket server will broadcast message to all the connected session.
+ */
+
 package com.sample.websocket.service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -8,13 +16,17 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/webecho")
 public class WebsocketServer {
-	int i = 0;
+
+	private static final Set<Session> sessionList = Collections.synchronizedSet(new HashSet<Session>());
 
 	@OnOpen
 	public void onOpen(Session session) {
 		System.out.println(session.getId() + " has opened a connection");
 		try {
-			session.getBasicRemote().sendText("Connection Established:::");
+			sessionList.add(session);
+			// session.getBasicRemote().sendText("Connection Established with
+			// id:::" + session.getId());
+			broadcastMessage("Connection Established with id:::" + session.getId());
 		} catch (Exception e) {
 			System.err.println("Error in opening connection:::");
 			e.printStackTrace();
@@ -25,14 +37,8 @@ public class WebsocketServer {
 	public void onMessage(String message, Session session) {
 		System.out.println("Message from " + session.getId() + " :::" + message);
 		try {
-			session.getBasicRemote().sendText(message);
-
-			while (i <= 10) {
-				Thread.sleep(2000);
-				System.out.println("Message ::" + i);
-				session.getBasicRemote().sendText("Message " + i);
-				i++;
-			}
+			// session.getBasicRemote().sendText(message);
+			broadcastMessage(session.getId() + ":::::" + message);
 		} catch (Exception e) {
 			System.err.println("Error in sending message");
 			e.printStackTrace();
@@ -41,6 +47,28 @@ public class WebsocketServer {
 
 	@OnClose
 	public void onClose(Session session) {
-		System.out.println("Session " + session.getId() + " has ended.");
+		try {
+			System.out.println("Session " + session.getId() + " has ended.");
+			sessionList.remove(session);
+			broadcastMessage("Session " + session.getId() + " has ended.");
+		} catch (Exception e) {
+			System.err.println("Error on closing connection:::" + session.getId());
+		}
+
+	}
+
+	/**
+	 * @param message
+	 *            Method to broad case message to all the sessions connected.
+	 */
+	private void broadcastMessage(String message) {
+		try {
+			for (Session ses : sessionList) {
+				ses.getBasicRemote().sendText(message);
+			}
+		} catch (Exception e) {
+			System.err.println("Error in sending message to all");
+			e.printStackTrace();
+		}
 	}
 }
